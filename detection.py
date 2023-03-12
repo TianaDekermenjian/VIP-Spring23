41b6e3bbc28992d36eb838d8d51342a9aa48d91b
Diana Dekermenjian <124739144+TianaDekermenjian@users.noreply.github.com>
2023-03-12 22:07:16 +0200

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,71 @@
+import os
+import pathlib
+import cv2
+from pycoral.utils import edgetpu
+from pycoral.utils import dataset
+from pycoral.adapters import common
+from pycoral.adapters import classify
+
+# Set camera resolution to ???x???, FPS to ???
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, ???)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, ???)
+cap.set(cv2.CAP_PROP_FPS, ???)
+
+# Specify the TensorFlow model, labels, and image
+script_dir = pathlib.Path(__file__).parent.absolute()
+model_file = os.path.join(script_dir, 'REPLACE')
+label_file = os.path.join(script_dir, 'REPLACE')
+
+# Initialize the TF interpreter
+interpreter = edgetpu.make_interpreter(model_file)
+interpreter.allocate_tensors()
+output_details = interpreter.get_output_details()
+
+while True:
+    # Capture a frame from the camera
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Resize the frame
+    size = common.input_size(interpreter)
+    image = cv2.resize(frame, size)
+
+    # Run an inference
+    common.set_input(interpreter, image)
+    interpreter.invoke()
+    classes = classify.get_classes(interpreter, top_k=1)
+
+    # Print the result
+    labels = dataset.read_label_file(label_file)
+    for c in classes:
+    print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
+
+    output_tensor = interpreter.get_tensor(output_details[0]['index'])
+    num_detections = int(output_tensor[0])
+
+    # Postprocess the output to get the bounding boxes and class labels
+    for i in range(num_detections):
+      class_id = int(output_tensor[1][i])
+      score = float(output_tensor[2][i])
+      bbox = output_tensor[3][i]
+      xmin = int(bbox[0] * frame.shape[1])
+      ymin = int(bbox[1] * frame.shape[0])
+      xmax = int(bbox[2] * frame.shape[1])
+      ymax = int(bbox[3] * frame.shape[0])
+      label = labels[class_id]
+
+      # Draw the bounding box and label on the frame
+      cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+      cv2.putText(frame, f'{label}: {score:.2f}', (xmin, ymin - 5),
+                  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Display the output frame on the screen
+    cv2.imshow('Object Detection', frame)
+    if cv2.waitKey(1) == ord('q'):
+      break
+
+# Release the VideoCapture object and close the window
+cap.release()
+cv2.destroyAllWindows()
