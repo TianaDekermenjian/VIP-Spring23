557cb0849a9ef43e8371a8ce5fd6ca744a9f6977
Diana Dekermenjian <124739144+TianaDekermenjian@users.noreply.github.com>
2023-03-26 14:14:28 +0300

    Update detection.py
    fixes in printing bbox

--- detection.py
+++ detection.py
@@ -1,12 +1,14 @@
 import os
 import pathlib
 import cv2
+import tracebck
 from pycoral.utils import edgetpu
 from pycoral.utils import dataset
 from pycoral.adapters import common
 from pycoral.adapters import classify
+from pycoral.adapters import detect
 
-# Set camera resolution to ???x???, FPS to ???
+# Set camera resolution and FPS accordingly
 cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -24,6 +26,8 @@ output_details = interpreter.get_output_details()
 
 try:
     while True:
+        # Measure inference time
+        st = time.perf_counter_ns()
         # Capture a frame from the camera
         ret, frame = cap.read()
         if not ret:
@@ -37,30 +41,27 @@ try:
         common.set_input(interpreter, image)
         interpreter.invoke()
         classes = classify.get_classes(interpreter, top_k=1)
+        
+        objs = detect.get_objects(interpreter, 0.4, [1,1])
 
         # Print the result
         labels = dataset.read_label_file(label_file)
         for c in classes:
-        print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
+            print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
 
         output_tensor = interpreter.get_tensor(output_details[0]['index'])
-        num_detections = int(output_tensor[0])
+        num_detections = len(output_tensor[0])
+        
+        for obj in objs:
+            print("obJTs")
+            print(labels.get(obj.id, obj.id))
+            print('  id:    ', obj.id)
+            print('  score: ', obj.score)
+            print('  bbox:  ', obj.bbox)
 
-        # Postprocess the output to get the bounding boxes and class labels
-        for i in range(num_detections):
-          class_id = int(output_tensor[1][i])
-          score = float(output_tensor[2][i])
-          bbox = output_tensor[3][i]
-          xmin = int(bbox[0] * frame.shape[1])
-          ymin = int(bbox[1] * frame.shape[0])
-          xmax = int(bbox[2] * frame.shape[1])
-          ymax = int(bbox[3] * frame.shape[0])
-          label = labels[class_id]
-
-          # Draw the bounding box and label on the frame
-          cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
-          cv2.putText(frame, f'{label}: {score:.2f}', (xmin, ymin - 5),
-                      cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.rectangle(frame, (obj.bbox.xmin, obj.bbox.xmax), (obj.bbox.ymin$, obj.bbox.ymax), (0, 255, 0), 2)
+            
+        print("Inference Time: ", (time.perf_counter_ns()-st) * 1e-6)
 
         # Display the output frame on the screen
         cv2.imshow('Object Detection', frame)
@@ -68,6 +69,9 @@ try:
           break
 except KeyboardInterrupt:
     pass
+except Exception as e:
+    print("Error", e)
+    traceback.print_exc()
 
 # Release the VideoCapture object and close the window
 cap.release()
