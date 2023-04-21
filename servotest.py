d7bb5e5792a657c74629608261b02d4800c27691
Diana <tiana.dekermenjian@gmail.com>
2023-04-21 17:31:15 +0300

    added loop to move servo

--- servotest.py
+++ servotest.py
@@ -1,13 +1,26 @@
 from periphery import PWM
+import time
 
-# Open PWM chip 0, channel 10
+# Open PWM pin
 pwm = PWM(2, 0)
 
-# Set frequency to 50 Hz
 pwm.frequency = 50
-# Set duty cycle to 5%
 pwm.duty_cycle = 0.05
 
 pwm.enable()
+
+# Define pulse widths for left, middle, and right positions
+left_pulse = 0.03
+middle_pulse = 0.07
+right_pulse = 0.11
+
+while True:
+    # Move to left position
+    pwm.duty_cycle = left_pulse
+    time.sleep(1)
+
+    # Move to right position
+    pwm.duty_cycle = right_pulse
+    time.sleep(1)
 
 pwm.close()
