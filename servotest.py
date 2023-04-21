63a84c56e19dacf76a11be2df42ba9c88393bc2c
Diana <tiana.dekermenjian@gmail.com>
2023-04-21 17:26:35 +0300

    fixed pwm pin

--- servotest.py
+++ servotest.py
@@ -3,14 +3,11 @@ from periphery import PWM
 # Open PWM chip 0, channel 10
 pwm = PWM(2, 0)
 
-# Set frequency to 1 kHz
-pwm.frequency = 1e3
-# Set duty cycle to 75%
-pwm.duty_cycle = 0.75
+# Set frequency to 50 Hz
+pwm.frequency = 50
+# Set duty cycle to 5%
+pwm.duty_cycle = 0.05
 
 pwm.enable()
-
-# Change duty cycle to 50%
-pwm.duty_cycle = 0.50
 
 pwm.close()
