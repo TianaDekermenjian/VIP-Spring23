6958afe810574209178837a809283b2735e2dde6
Diana <tiana.dekermenjian@gmail.com>
2023-05-04 18:01:46 +0300

    upload videos and finalize

--- upload.py
+++ upload.py
@@ -35,16 +35,13 @@ def upload_thread():
                 os.remove(frame_file)
             isFinished = False
 
-# def in_between():
-#    asyncio.run(upload_thread())
-
 @sio.on('connect')
 def on_connect():
     print('Connected!')
 
 @sio.on('start_recording')
 def on_start():
-    global isRecording, isFinished
+    global isRecording
     print("Started")
     isRecording = True
     # Set the camera resolution and frame rate
@@ -61,35 +58,28 @@ def on_start():
     out = cv2.VideoWriter(f'/home/mendel/VIP/frames/output.mp4', fourcc, fps, resolution)
 
     # Record the video
-    start_time = time.time()
-    # index = 0
-    while isRecording and time.time() - start_time < 5:
+    while isRecording:
         ret, frame = camera.read()
-        #print('it is recording')
         if not ret:
             break
 
-        # index = index +1
-        #cv2.imwrite(f'/home/mendel/VIP/frames/frame{index}.png', frame)
         out.write(frame)
 
         cv2.waitKey(1)
-    stop_time = time.time()
-    duration = stop_time - start_time
-    print(duration)
 
     # Release the camera and destroy the window when all recordings are complete
     camera.release()
     out.release()
-    isFinished = True
+
     print('recording is done')
     cv2.destroyAllWindows()
 
 @sio.on('stop_recording')
 def on_stop():
-    global isRecording
+    global isRecording, isFinished
     print("Stopped")
     isRecording = False
+    isFinished = True
 
 # Testing app
 sio.connect('http://ec2-52-91-118-179.compute-1.amazonaws.com:3001')
