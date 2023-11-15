53b5d35c275b2350ce57b16777aa5956dd5d7eb0
Diana <tiana.dekermenjian@gmail.com>
2023-11-15 13:59:22 +0200

    separated video writers

--- upload_test.py
+++ upload_test.py
@@ -12,14 +12,10 @@ def start_recording(times):
 
     # Initialize the ArduCam USB camera
     camera = cv2.VideoCapture(1)
-    #camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"mp4v"))
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
     camera.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
     camera.set(cv2.CAP_PROP_FPS, fps)
 
-    # Initialize the video writer with the output filename and codec
-
-    # Record the video and display the frames in a window
     start_time = time.time()
     start_time2 = time.time()
 
@@ -27,10 +23,16 @@ def start_recording(times):
 
     index = 0
     filename = f"/home/mendel/VIP/VIP-Spring23/video_{index}.mp4"
+
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     writer = cv2.VideoWriter(filename, fourcc, fps, resolution)
+
     while time.time() - start_time < times:
-        filename = f"./Video/video_{index}.mp4"
+        ret, frame = camera.read()
+        if not ret:
+            break
+        writer.write(frame)
+
         if time.time() - start_time2 >= 10:
             # Release the video writer and display a message when the recording is complete
             writer.release()
@@ -38,31 +40,15 @@ def start_recording(times):
             print(f"Video recording saved as:, {filename}")
             measure1.append(time.time())
 
-            # Initialize the ArduCam USB camera
-            camera = cv2.VideoCapture(1)
-            camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"mp4v"))
-            camera.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
-            camera.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
-            camera.set(cv2.CAP_PROP_FPS, fps)
-
-            # Construct the filename with a timestamp and save it to the desired directory
-
-            # Initialize the video writer with the output filename and codec
-            fourcc = cv2.VideoWriter_fourcc(*"mp4v")
             writer = cv2.VideoWriter(filename, fourcc, fps, resolution)
 
             measure2.append(time.time())
             start_time2 = time.time()
 
-        ret, frame = camera.read()
-        if not ret:
-            break
-        writer.write(frame)
-        # Wait for a short time to display the frames in the window
         cv2.waitKey(1)
+
     else:
         writer.release()
-        filename = f"./Video/video_{index}.mp4"
         index += 1
         print(f"Video recording saved as:, {filename}")
 
