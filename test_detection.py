33942b8a22d2e0364cee8a71d0e9a28508609311
Diana <tiana.dekermenjian@gmail.com>
2023-12-20 12:24:10 +0000

    real time test on court

--- test_detection.py
+++ test_detection.py
@@ -10,7 +10,7 @@ from pycoral.adapters import detect
 
 # Specify the TensorFlow model, labels, and image
 script_dir = pathlib.Path(__file__).parent.absolute()
-model_file = os.path.join(script_dir, 'models (edgetpu)/model1_edgetpu.tflite')
+model_file = os.path.join(script_dir, 'models (edgetpu)/last_full_integer_quant_edgetpu.tflite')
 label_file = os.path.join(script_dir, 'sunnylabels.txt')
 
 images_path = os.path.join(script_dir, 'test')
@@ -26,7 +26,7 @@ output_details = interpreter.get_output_details()
 # Resize the image
 size = common.input_size(interpreter)
 image = cv2.imread(INPUT_IMAGE)
-image = cv2.resize(image, (640, 640))
+image = cv2.resize(image, (416, 416))
 
 # Run an inference
 common.set_input(interpreter, image)
@@ -37,13 +37,21 @@ input_tensor = interpreter.get_input_details()
 print("input tensor: ")
 print(input_tensor)
 
-output_tensor = interpreter.get_tensor(output_details[0]['index'])
+output_tensor = interpreter.get_tensor(output_details[0]['index'])[0]
+print("output tensor: ")
 print(output_tensor)
+
+print("num detections:")
+print(len(output_tensor))
 
 # Print the result
 labels = dataset.read_label_file(label_file)
+print("labels: ")
+print(labels)
+
 for c in classes:
-  print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
+   print(c)
+#  print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
 
 #for obj in objs:
 #  print("hello")
