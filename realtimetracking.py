dad8993f4e0cd4e5683dd1f8e589e8f4fc211c18
Diana <tiana.dekermenjian@gmail.com>
2024-02-25 22:46:30 +0000

    weights test

--- realtimetracking.py
+++ realtimetracking.py
@@ -17,7 +17,8 @@ parser.add_argument("--display", "-d", action='store_true', help="Display detect
 parser.add_argument("--stream", "-s", action='store_true', help="Process video stream in real-time")
 parser.add_argument("--conf", "-ct", type=float, default=0.5, help="Detection confidence threshold")
 parser.add_argument("--iou", "-it", type=float, default=0.1, help="Detections IOU threshold")
-
+parser.add_argument("--wb", "-b", type=int, default=10, help = "Weight of basketball")
+parser.add_argument("--wp", "-p", type=int, default=7, help = "Weight of player")
 args = parser.parse_args()
 
 model = YOLOv5s(args.model, args.labels, args.conf, args.iou)
@@ -37,15 +38,30 @@ if(args.image) is not None:
 
     detections = model.postprocess(output)
 
+    wt = 0
+
+    height, width, _ = img.shape
+
     for detection in detections:
         x1, y1, x2, y2, conf, class_id = detection
 
         c = int(class_id)
 
+        if c == 0:
+            wt += args.wb
+        elif c == 1:
+            wt += args.wp
+
         label = f'{classes[c]} {conf:.2f}'
+        weight = f"Weight of frame: {wt}"
+
+        (text_width, text_height), baseline = cv2.getTextSize(weight, 0, 1, 2)
+        text_x = int((width - text_width) / 2)
+        text_y = text_height + 10
 
         cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), [0, 0, 255], 2)
         cv2.putText(img, label, (int(x1), int(y1-2)), 0, 0.5, (255, 255, 255), 1, lineType=cv2.LINE_AA)
+        cv2.putText(img, weight, (text_x, text_y), 0, 0.5, (255, 255, 255), 1, lineType=cv2.LINE_AA)
 
     s = ""
 
